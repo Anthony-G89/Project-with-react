f77257c9245ef22fb0334e5581dac583e3fd7175
Anthony Guerrero <adguerrero1935@gmail.com>
2020-08-04 17:43:29 -0400

    Trying to create a fuction to close my form

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,13 +1,54 @@
 import React from "react";
+import "./style.css";
 
 
-const blog = propz => {
+class Form extends React.Component {
 
+    // const closingForm = () => {
+    //    alert("clicked on")
+    // }
+
+    constructor() {
+        super()
+        this.state= {
+            showMe: true
+        }
+    }
+
+    closeWindow()
+    {
+        alert("clicked on")
+    }
+
+
+
+    render() {
     return (
         <div>
-            <h1 className="text-center home">This is my blog page</h1>
+            
+            <form className="userForm">
+            <i className="fas fa-window-close closeWindow" aria-hidden="true" ></i>
+                <div className="form-group">
+                    <label className="userEmail">Email Address</label>
+                    <input type="email" className="form-control userInput" />
+                    <smal className="emailHelp">We'll never share your email with anyone else.</smal>
+                </div>
+                <div className="form-group">
+                    <label className=" userEmail">Password</label>
+                    <input type="password" className="form-control userPassword" />
+                </div>
+                <div className="form-check pl-0">
+                    <label className="form-check">
+                        <input type="checkbox" />Remember Me
+                    </label>
+                    <button type="submit" className=" btn btn-primary">Submit</button>
+
+                </div>
+
+            </form>
         </div>
     )
+    }
 }
 
-export default blog;
+export default Form;
