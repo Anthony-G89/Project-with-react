222be55e8e544498a4e0ef116d7f576e46d36a31
Anthony Guerrero <adguerrero1935@gmail.com>
2020-08-04 19:22:51 -0400

    trying to work on closing form

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,8 +5,8 @@ const about = propz => {
 
     return (
         <div>
-
             <h1 className="text-center" >This is my about page</h1>
+         
         </div>
     )
 }
--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -8,46 +8,46 @@ class Form extends React.Component {
     //    alert("clicked on")
     // }
 
-    constructor() {
-        super()
-        this.state= {
-            showMe: true
-        }
-    }
+    // constructor() {
+    //     super()
+    //     this.state= {
+    //         showMe: true
+    //     }
+    // }
 
-    closeWindow()
-    {
+    Window() {
         alert("clicked on")
     }
+    
+   
 
 
 
     render() {
-    return (
-        <div>
-            
-            <form className="userForm">
-            <i className="fas fa-window-close closeWindow" aria-hidden="true" ></i>
-                <div className="form-group">
-                    <label className="userEmail">Email Address</label>
-                    <input type="email" className="form-control userInput" />
-                    <smal className="emailHelp">We'll never share your email with anyone else.</smal>
-                </div>
-                <div className="form-group">
-                    <label className=" userEmail">Password</label>
-                    <input type="password" className="form-control userPassword" />
-                </div>
-                <div className="form-check pl-0">
-                    <label className="form-check">
-                        <input type="checkbox" />Remember Me
+        return (
+            <div>
+                <form className="userForm">
+                <i className="fas fa-window-close closeWindow"  onClick={this.Window} ></i>
+                    <div className="form-group">
+                        <label className="userEmail">Email Address</label>
+                        <input type="email" className="form-control userInput" />
+                        <smal className="emailHelp">We'll never share your email with anyone else.</smal>
+                    </div>
+                    <div className="form-group">
+                        <label className=" userEmail">Password</label>
+                        <input type="password" className="form-control userPassword" />
+                    </div>
+                    <div className="form-check pl-0">
+                        <label className="form-check">
+                            <input type="checkbox" />Remember Me
                     </label>
-                    <button type="submit" className=" btn btn-primary">Submit</button>
+                        <button type="submit" className=" btn btn-primary">Submit</button>
 
-                </div>
+                    </div>
 
-            </form>
-        </div>
-    )
+                </form>
+            </div>
+        )
     }
 }
 
