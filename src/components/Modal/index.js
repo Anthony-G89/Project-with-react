0ee4c5a76e3ba3b5ce902280e01b2220a0d81d23
Anthony Guerrero <adguerrero1935@gmail.com>
2020-08-25 18:25:54 -0400

    Added a picture and some styling to the modal content

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -26,6 +26,7 @@ const Modal = ({ show, close }) => {
                 <div className="modal-body">
                     <h3>SURPRISE!!</h3>
                     <img src={person} className="surprised"  />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget justo eu elit accumsan sodales. Duis blandit maximus nisi eu pellentesque. Fusce pharetra quam nec leo auctor, nec varius lorem condimentum. Cras fermentum nulla purus, vel aliquam ante convallis sit amet. Donec commodo leo ante, non tristique magna semper et. Phasellus metus tellus, aliquet in lacus sed, volutpat maximus felis. Duis accumsan molestie sapien, sed gravida arcu iaculis eu. Proin lacinia orci a elit scelerisque, id pretium leo auctor.</p>
                 </div>
                 <div className="modal-footer">
                     <button onClick={close} className="close-modal-btn">Close</button>
